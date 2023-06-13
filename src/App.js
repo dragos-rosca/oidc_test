c39a54cda1c9772a08a9ec673bf5075e65f04c15
drosca@totalsoft.ro <drosca@totalsoft.ro>
2023-06-13 14:41:01 +0300

    last changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { OidcSecure, useOidcUser, useOidc } from "@axa-fr/react-oidc";
+import { useCallback } from "react";
 
 function App() {
+  const t = localStorage.getItem("tenant") || "default";
+
+  const { oidcUser } = useOidcUser(t);
+  const { logout } = useOidc(t);
+  const onLogout = useCallback(() => logout(), [logout]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <OidcSecure configurationName={t}>
+      <div>
+        <h1>{oidcUser?.tid}</h1>
+        <button onClick={onLogout}>Logout</button>
+      </div>
+    </OidcSecure>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useCallback, useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { OidcProvider } from "@axa-fr/react-oidc";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const urlRoot = `${window.location.protocol}//${window.location.hostname}${
+  window.location.port ? `:${window.location.port}` : ""
+}`;
+const AUTH = {
+  CALLBACK: "/authentication/callback",
+  SILENT_CALLBACK: "/authentication/silent_callback",
+};
+
+const config = (t) => {
+  const extras = t === "default" ? undefined : { acr_values: `tenant:${t}` };
+  return {
+    client_id: "eOrder_Frontend",
+    authority: "https://sso.appservice.online",
+    redirect_uri: `${urlRoot}${AUTH.CALLBACK}`,
+    silent_redirect_uri: `${urlRoot}${AUTH.SILENT_CALLBACK}`,
+    response_type: "code",
+    scope: "openid profile IdentityServerApi email roles",
+    post_logout_redirect_uri: `${urlRoot}`,
+    automaticSilentRenew: true,
+    loadUserInfo: true,
+    triggerAuthFlow: true,
+    revokeAccessTokenOnSignout: true,
+    register_uri: `https://sso.appservice.online/?returnUrl=${urlRoot}/register&clientId=eOrder_Frontend`,
+    extras,
+  };
+};
+
+const AuthProvider = ({ children }) => {
+  const t = localStorage.getItem("tenant") || "default";
+
+  const [cfg, setCfg] = useState(config(t));
+
+  const onClick = useCallback(() => {
+    localStorage.setItem("tenant", "11F38BA7-6F50-48F0-B8B6-109A2AE294FD");
+    setCfg(config("11F38BA7-6F50-48F0-B8B6-109A2AE294FD"));
+  }, []);
+  const onClick2 = useCallback(() => {
+    localStorage.setItem("tenant", "DA84628A-2925-4B69-9116-A90DD5A72B1F");
+    setCfg(config("DA84628A-2925-4B69-9116-A90DD5A72B1F"));
+  }, []);
+
+  const logEvent = useCallback((cfgName, name, info) => {
+    console.log(`${cfgName} : ${name}`);
+    console.log(info);
+  }, []);
+
+  return (
+    <OidcProvider configuration={cfg} configurationName={t} onEvent={logEvent}>
+      <button onClick={onClick}>Change Tenant</button>
+      <button onClick={onClick2}>Change Tenant</button>
+      {children}
+    </OidcProvider>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
 
